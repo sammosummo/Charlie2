7dd562e180e6ea8e830a9231f42e47a0ce730df3
sammosummo <samuel.mathias@gmail.com>
2018-07-10 15:49:34 -0400

    basic tests all working

--- charlie2/tools/app.py
+++ charlie2/tools/app.py
@@ -3,7 +3,7 @@
 """
 from sys import argv, exit
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 from charlie2.tools.mainwindow import MainWindow
 from charlie2.tools.paths import logo_path
@@ -14,5 +14,6 @@ def run_app():
     app = QApplication(argv)
     app.setWindowIcon(QIcon(logo_path))
     app.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    app.setFont(QFont("Sanserif", 18))
     ex = MainWindow()
     exit(app.exec_())
